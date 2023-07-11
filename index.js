404c7f593ee523415deda4586c17819b663ce115
Balanescu Raul <balanescuraul@gmail.com>
2023-07-11 19:21:30 +0300

    add examples

---
+++ index.js
@@ -0,0 +1,30 @@
+// synchronous 
+
+// console.log(" I ");
+
+// console.log(" eat");
+
+// console.log (" ice cream");
+
+// console.log(" with a");
+
+// console.log(" spoon");
+
+// asynchronous 
+
+console.log(" I ");
+
+console.log(" eat");
+
+console.log (" ice cream");
+
+
+setTimeout(() => {
+    
+console.log (" ice cream");
+},3000);
+
+console.log(" with a");
+
+console.log(" spoon");
+
